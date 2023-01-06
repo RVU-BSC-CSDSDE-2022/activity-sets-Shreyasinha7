2d097c0106c37a16d200b0f6b1d4ba6a7e2c9a6c
Shreya  Sinha <115694115+Shreyasinha7@users.noreply.github.com>
2023-01-06 07:08:34 +0000

    solved

--- set01/problem08.c
+++ set01/problem08.c
@@ -2,10 +2,34 @@
 int input_array_size();
 void input_array(int n, int a[n]);
 int sum_n_array(int n, int a[n],int sum);
+void output(int n, int a[n],int sum);
 int input_array_size()
 {
   int n;
   printf("enter the array size:");
   scanf("%d",&n);
   return(n);
+}
+void input_array(int n, int a[n])
+{
+  int i;
+  for(i=0;i<n;i++)
+  {
+  scanf("%d",&a[i]);  
+  }
+}
+int sum_n_array(int n, int a[n])
+{
+  int i,sum=0;
+  for(i=0;i<n;i++)
+    {
+      sum=sum+a[i];
+    }
+  return(sum);
+}
+void output(int n, int a[n],int sum)
+{
+  for (int i=0; i<n-1;i++){
+    printf("%d")
+  }
 }
