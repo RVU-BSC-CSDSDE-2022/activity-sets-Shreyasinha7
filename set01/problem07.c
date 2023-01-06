c299fe3169843a1d1927975a035714ac11e8c58a
Shreya  Sinha <115694115+Shreyasinha7@users.noreply.github.com>
2023-01-06 04:40:46 +0000

    solved

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
-void add(int x, int y, int *sum);
+int input_n();
+int sum_n_nos(int n);
+void output(int n, int sum);
+int input()
+{
+  int n;
+  printf("enter the number:");
+  scanf("%d",&n);
+  return n;
+}
+int sum_n_nos( int n)
+{
+  int sum=0,i;
+  i=0;
+  while(i<=n)
+    {
+      i++;
+      sum=sum+i;
+    }
+  return(sum);
+}
+void output(int n, int sum)
+{
+  printf("the sum of first %d natural numbers is %d",n,sum);
+}
 int main()
 {
-      int x=8,y=9, sum=0;
-      sum=x+y;
-      add(x, y, &sum);
-      printf("sum is %d", sum);
-      return 0;
+  int n,i,sum;
+  n=input();
+  sum=sum_n_nos(n);
+  output(n,sum);
+  return(0);
 }
-   void  add(int x, int y, int *sum)
-   {
-         *sum=x+y;
-    }
