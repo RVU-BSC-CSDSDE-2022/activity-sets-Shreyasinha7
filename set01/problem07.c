a06f2d9beb5c1b3f2e042f4a23774835b8eea157
Shreya  Sinha <115694115+Shreyasinha7@users.noreply.github.com>
2023-02-01 18:16:20 +0000

    solved

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,34 +1,35 @@
-#include <stdio.h>
+#include<stdio.h>
 int input_n();
 int sum_n_nos(int n);
 void output(int n, int sum);
-int input()
+int input_n()
 {
-  int n;
-  printf("enter the number:");
-  scanf("%d",&n);
-  return n;
+ int n;
+ printf("enter the number:");
+ scanf("%d",&n);
+ return n;
 }
-int sum_n_nos( int n)
+int sum_n_nos(int n)
 {
-  int sum=0,i;
-  i=0;
-  while(i<=n)
-    {
-      i++;
-      sum=sum+i;
-    }
+ int sum=0,i;
+ i=0;
+ while(i<n)
+   {
+    i++;
+    sum=sum+i;
+   }
   return(sum);
 }
-void output(int n, int sum)
+void output(int n , int sum)
 {
-  printf("the sum of first %d natural numbers is %d",n,sum);
-  }
+ printf("the sum of the first %d natural numbers is %d",n,sum);
+}
 int main()
 {
-  int n,i,sum;
-  n=input();
-  sum=sum_n_nos(n);
-  output(n,sum);
-  return(0);
+ int n,i,sum;
+ n=input_n();
+ sum=sum_n_nos(n);
+ output(n,sum);
+ return(0);
 }
+
--- set01/problem09.c
+++ set01/problem09.c
@@ -1,37 +1,33 @@
-#include <math.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
 float input();
 float square_root(float n);
 void output(float n, float sqrroot);
 float input()
 {
-  float n ,sqrroot;
-  printf("enter the number:");
-  scanf("%f", &n);
-  return(n);
-}  
+float n,sqrroot;
+printf("enter the number:");
+scanf("%f",&n);
+return(n);
+}
 float square_root(float n)
 {
-  float i=1;
-  while(fabs(i*i-n)/2>0.0001)
-    {
-      i=(i+n/i)/2;
-    }
-  return i;
+float i=1;
+while(fabs(i*i-n)/2>0.0001)
+{
+i=(i+n/i)/2;
+}
+return i;
 }
 void output(float n, float sqrroot)
 {
-  printf("square root of5.2f=%.2f", n,sqrroot);
-}
-void output(float n ,float sqrroot)
-{
-  printf("Square root of %.2lf =  %.2lf", n , sqrroot);
+printf("square root of%.2f=%.2f", n,sqrroot);
 }
 int main()
 {
-  int n,sqrroot;
-  n=input();
-  sqrroot=square_root(n);
-  output(n,sqrroot);
-  return(0);
+int n ,sqrroot;
+n=input();
+sqrroot=square_root(n);
+output(n,sqrroot);
+return(0);
 }
