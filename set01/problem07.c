f606e5f4c0ac6551424e66756cbd43ed3ab34308
Shreya  Sinha <115694115+Shreyasinha7@users.noreply.github.com>
2023-03-07 12:11:03 +0000

    solved

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,35 +1,30 @@
-  #include<stdio.h>
-int input_n();
+#include <stdio.h>
+
+int input();
 int sum_n_nos(int n);
 void output(int n, int sum);
-int input_n()
+int input()
 {
- int n;
- printf("enter the number:");
- scanf("%d",&n);
- return n;
+  int n;
+  printf("Enter 'n'\n");
+  scanf("%d",&n);
+  return n;
 }
+
 int sum_n_nos(int n)
 {
- int sum=0,i;
- i=0;
- while(i<n)
-   {
-    i++;
-    sum=sum+i;
-   }
-  return(sum);
+  return (n*(n+1))/2;
 }
-void output(int n , int sum)
+
+void output(int n, int sum)
 {
- printf("the sum of the first %d natural numbers is %d",n,sum);
+  printf("Sum of first %d natural numbers is %d",n,sum);
 }
 int main()
 {
- int n,i,sum;
- n=input_n();
- sum=sum_n_nos(n);
- output(n,sum);
- return(0);
+  int n,sum;
+  n=input();
+  sum=sum_n_nos(n);
+  output(n,sum);
+  return 0;
 }
-
